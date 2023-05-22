01b036685deb2429387edae225a0fb330c845bed
EUNSEO814 <eunseo814@gmail.com>
2023-05-22 23:02:37 +0900

    ⚡️ Header, useAnimation, useScroll

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,23 @@ const Home = () => {
   return (
     <div>
       <h1 style={{ paddingTop: "100px" }}>home</h1>
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
+      <h1 style={{ paddingTop: "100px" }}>home</h1>
     </div>
   );
 };
