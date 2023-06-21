e8749015e730130bd3af1e0a0aef5d63e64f4157
EUNSEO814 <eunseo814@gmail.com>
2023-06-21 21:52:13 +0900

    ⚡️ api

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,24 +1,12 @@
+import { useQuery } from "react-query";
+import { getMovies } from "../api";
+
 const Home = () => {
+  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  console.log(data, isLoading);
   return (
     <div>
-      <h1 style={{ paddingTop: "100px" }}>home</h1>
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>{" "}
-      <h1 style={{ paddingTop: "100px" }}>home</h1>
+      <h1 style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</h1>
     </div>
   );
 };
